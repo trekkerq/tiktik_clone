10e0a6d85d4492643c6e5aa0c9fd77cc48aa91c0
trekkerq <55770904+trekkerq@users.noreply.github.com>
2022-09-05 17:56:06 -0400

    ...

--- components/Discover.tsx
+++ components/Discover.tsx
@@ -14,7 +14,7 @@ const Discover = () => {
     <div className="xl:border-b-2 xl:border-gray-200 pb-6">
       <p className="text-gray-500 font-semibold m-3 
       mt-4 hidden xl:block">Popular Marketplaces</p>
-      <div className="flex gap-3 flex-wrap justify-between">
+      <div className="flex gap-3 flex-wrap justify-content">
         {topics.map((item) => (
             <Link href={`/?topic=${item.name}`}
              key={item.name}>
